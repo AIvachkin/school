967257710053331abf5df2d939a1e67c1e00968b
Андрей <andrews.ru83@gmail.com>
2022-09-11 14:43:25 +0300

    Созданы сервисы, контроллеры с четырьмя CRUD-эндпоинтами + фильтрация + 3.3 База Данных + фильтрация в репозитории v2.0 + классы тестов (не сделано)

---
+++ src/test/java/ru/hogwarts/school/constants/StudentServiceConstants.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.constants;
+
+import ru.hogwarts.school.model.Student;
+
+
+public class StudentServiceConstants {
+
+    public static Student STUDENT1 = new Student(1, "Oleg", 20);
+    public static Student STUDENT2 = new Student(2, "Anna", 22);
+    public static Student STUDENT3 = new Student(3, "Michael", 25);
+    public static Student STUDENT4 = new Student(4, "Andrew", 22);
+    public static Student STUDENT5 = new Student(5, "Olga", 27);
+}
---
+++ src/test/java/ru/hogwarts/school/service/StudentServiceTest.java
@@ -0,0 +1,61 @@
+package ru.hogwarts.school.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.hogwarts.school.constants.StudentServiceConstants;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static ru.hogwarts.school.constants.StudentServiceConstants.*;
+
+//@ExtendWith(MockitoExtension.class)
+//public class StudentServiceTest {
+//
+////    @Mock
+////    private final StudentRepository studentRepositoryMock = mock(StudentRepository.class);
+////
+////    @InjectMocks
+////    private StudentService out;
+////
+////    @BeforeEach
+////    public void initOut(){
+////        out = new StudentService(studentRepositoryMock);
+////    }
+//
+//    @Mock
+//    private final StudentRepository studentRepositoryMock = mock(StudentRepository.class);
+//
+//    @InjectMocks
+//    private StudentService studentService = new StudentService(studentRepositoryMock);
+//
+//
+//    @Test
+//    public void createStudentTest() {
+//
+//
+//        when(studentService.createStudent(STUDENT1)).thenReturn(STUDENT1);
+////        when(studentService.findStudent(1)).thenReturn(STUDENT1);
+////        when(studentService.editStudent(1, STUDENT2)).thenReturn(STUDENT2);
+////        when(studentServiceMoteStudent(STUDENT5)).thenReturn(STUDENT5);
+////        Optional<Student> opt = Optional.ofNullable(studentService.findStudent(1));
+////        Student actualStudent = opt.get();
+//
+//        assertEquals (STUDENT1, studentService.createStudent(STUDENT1));
+//
+////        assertEquals (STUDENT1, actualStudent);
+//
+//    }
+//
+//}
