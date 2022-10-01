db5962d15d3e68218a18ec5d827f4143c4d60fa3
Андрей <andrews.ru83@gmail.com>
2022-10-01 22:04:48 +0300

    Созданы сервисы, контроллеры с четырьмя CRUD-эндпоинтами + фильтрация + 3.3 База Данных + фильтрация в репозитории v2.0 + классы тестов (не сделано) + 3.4 (не полностью) + доработка 2.0 + 3.5 + после разбора 1 + 3.6 (не до конца) + 3.6 (разбор) + Видеоразбор (ничего не работает) + 2.0 + 4.1 @Query и пагинация + разбор + 4.2 Управление схемами баз данных + 4.2 (не работает) + работает

--- src/main/resources/liquibase/scripts/lesson-four-three.sql
+++ src/main/resources/liquibase/scripts/lesson-four-three.sql
@@ -1,7 +1,7 @@
-- liquibase formatted sql
+--liquibase formatted sql
 
-- changeset aivachkin:1
-CREATE INDEX search_for_name_idx ON student (name);
+--changeset aivachkin:1
+create index search_for_name_idx ON student (name);
 
-- changeset aivachkin:2
-CREATE INDEX search_for_name_and_color_idx ON faculty (name, color);
+--changeset aivachkin:2
+create index search_for_name_and_color_idx ON faculty (name, color);
